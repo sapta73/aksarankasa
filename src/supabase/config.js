8e68f35ce595fdda93704c2a6cdeda4d5c3e8a7a
SAPTA ALIANSYAH PUTRA <saptaaliansyahputra123@gmail.com>
2025-07-29 19:04:10 +0700

    Update config.js
    fix database

--- src/supabase/config.js
+++ src/supabase/config.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// GANTI DENGAN URL DAN KUNCI DARI DASHBOARD SUPABASE ANDA!
+
 const supabaseUrl = "https://rqzocgbovysrpozmehlb.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxem9jZ2JvdnlzcnBvem1laGxiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMwMjc4NTUsImV4cCI6MjA2ODYwMzg1NX0.W7TfPu7hYxAAynH2yCW4ZD1vUTI-RGWYHS2X-7CXKvE";
